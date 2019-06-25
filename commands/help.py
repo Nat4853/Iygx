82e291d4d43438a420a85a1f8b1651d2fe4d89fe
Nat <nathan.l0823@gmail.com>
2019-06-25 17:55:59 +0100

    Update help.py
    Added first version of the help menu.

--- commands/help.py
+++ commands/help.py
@@ -8,6 +8,12 @@ class Help:
     @commands.command(name="help", aliases=["commands"])
     async def help(self, ctx):
         if ctx.message.author.bot: return
+        s = "          "
+        embed = discord.Embed(colour=0xf3ebfb, description=
+            f"__**Iygx Help and Commands Menu.**__\n\n\
+             **,help** / **,commands**{s}Both aliases of this command open this menu.\n\
+             **,ttt**{s}This command begins a classic game of Tic Tac Toe. You can ~~choose to either~~ play with ~~an AI or~~ another member of this server.")
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Help(bot))
