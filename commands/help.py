8eb9ddbb9dbedd3ce74c098c7fb4e4cb4433dbf0
Nat <nathan.l0823@gmail.com>
2019-06-25 15:31:47 +0100

    Update help.py
    Change out tabs for spaces.

--- commands/help.py
+++ commands/help.py
@@ -2,12 +2,12 @@ import discord
 from discord.ext import commands
 
 class Help:
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.command(name="help", aliases=["commands"])
-	async def help(self, ctx):
-		if ctx.message.author.bot: return
+    @commands.command(name="help", aliases=["commands"])
+    async def help(self, ctx):
+        if ctx.message.author.bot: return
 
 def setup(bot):
-	bot.add_cog(Help(bot))
+    bot.add_cog(Help(bot))
